dc5109e6c9ef5c1454f9b4a253f9c22b5d91c817
PoetNet <marfutin.00@mail.ru>
2024-05-01 12:41:30 +0300

    Resolve a daily task

---
+++ Console/Program.cs
@@ -0,0 +1,11 @@
+﻿//1.PartReverser
+
+using Tasks.Daily.April2024;
+
+var reverser = new PartReverser();
+
+var case1 = reverser.ReversePrefix1("abcdefd", 'd'); //expected "dcbaefd"
+var case2 = reverser.ReversePrefix1("xyxzxe", 'z');  //expected "zxyxxe"
+var case3 = reverser.ReversePrefix1("abcd", 'z');    //expected "abcd"
+
+Console.WriteLine($"{case1}\n{case2}\n{case3} ");
---
+++ Tasks/Daily/April2024/1.PartReverser.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Tasks.Daily.April2024;
+
+public class PartReverser
+{
+    public string ReversePrefix1(string word, char ch)
+    {
+        var chIndex = word.IndexOf(ch);
+        if (chIndex == -1 || chIndex == 0) return word;
+
+        var charArray = word.ToCharArray();
+        int left = 0;
+        int right = chIndex;
+
+        while (left < right)
+        {
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] =
+                temp; // is 5% (64мс/61мс) slower: `(charArray[left], charArray[right]) = (charArray[right], charArray[left]);` 
+            left++;
+            right--;
+        }
+
+        return new string(charArray);
+    }
+
+    public string ReversePrefix2(string word, char ch) // is 5% slower than first too
+    {
+        var arr = word.ToCharArray();
+        var idx = word.IndexOf(ch);
+        Array.Reverse(arr, 0, idx + 1);
+        return new string(arr);
+    }
+}
