2c0cafaeb0e9a4b7a9b01e4f345ca34e41f2c555
PoetNet <marfutin.00@mail.ru>
2024-05-05 20:28:18 +0300

    Decide MatrixTransposer

---
+++ Tasks/Daily/April2024/5.MatrixTransposer.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.Daily.April2024;
+
+public class MatrixTransposer
+{
+    public int[][] Transpose(int[][] matrix)
+    {
+        int rows = matrix[0].Length;
+        int cols = matrix.Length;
+
+        var result = new int[rows][];
+
+        for (int i = 0; i <= rows; i++)
+        {
+            result[i] = new int[cols];
+            for (int j = 0; j <= cols; j++)
+            {
+                result[i][j] = matrix[j][i];
+            }
+        }
+
+        return result;
+    }
+}
