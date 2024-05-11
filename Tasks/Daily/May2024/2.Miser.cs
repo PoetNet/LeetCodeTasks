ff4ea6dd3a7efa909c8606348dddeb4debddb9c8
PoetNet <marfutin.00@mail.ru>
2024-05-11 11:47:33 +0300

    Decide Miser

---
+++ Tasks/Daily/May2024/2.Miser.cs
@@ -0,0 +1,86 @@
+﻿namespace Tasks.Daily.May2024;
+
+public static class Miser
+{
+    //public static double MincostToHireWorkers(int[] quality, int[] wage, int k)
+    //{
+    //    int n = quality.Length;
+    //    var totalCost = double.MaxValue;
+    //    var sortedRatio = SortRatio(CalculateWageToQualityRatio(quality, wage));
+
+    //    var highestQualityWorkers = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y - x));
+    //    int currentTotalQuality = 0;
+
+    //    for (int i = 0; i < sortedRatio.Count; i++)
+    //    {
+    //        var current = sortedRatio[i];
+    //        highestQualityWorkers.Enqueue(current.Item2, current.Item2);
+    //        currentTotalQuality += current.Item2;
+
+    //        if (highestQualityWorkers.Count() > k)
+    //        {
+    //            highestQualityWorkers.Sort();
+    //            highestQualityWorkers.Remove(current.Item2);
+    //            currentTotalQuality -= highestQualityWorkers.Last();
+    //        }
+
+    //        if (highestQualityWorkers.Count() == k)
+    //        {
+    //            totalCost = Math.Min(totalCost, currentTotalQuality * sortedRatio[i].Item1);
+    //        } 
+    //    }
+
+    //    return totalCost;
+    //}
+
+    //public static List<(double, int)> CalculateWageToQualityRatio(int[] quality, int[] wage)
+    //{
+    //    List<(double, int)> ratio = new();
+    //    for (int i = 0; i < quality.Length; i++)
+    //    {
+    //        ratio.Add(((double)wage[i] / quality[i], quality[i]));
+    //    }
+
+    //    return ratio;
+    //}
+
+    //public static List<(double, int)> SortRatio(List<(double, int)> ratio) =>
+    //    ratio.OrderByDescending(x => x.Item1).ToList();
+
+    public static double MincostToHireWorkers(int[] quality, int[] wage, int k)
+    {
+        var n = quality.Length;
+        var q1 = new PriorityQueue<(int, int), double>();
+
+        for (int i = 0; i < n; i++)
+        {
+            q1.Enqueue((quality[i], wage[i]), (1.0 * wage[i]) / quality[i]);
+        }
+
+        var result = double.MaxValue;
+        var sum = 0.0;
+        var q2 = new PriorityQueue<double, double>();
+
+        while (q1.Count > 0)
+        {
+            var (ql, wg) = q1.Dequeue();
+            var r = wg / (1.0 * ql);
+
+            sum += ql;
+
+            q2.Enqueue(-ql, -ql);
+
+            if (q2.Count > k)
+            {
+                sum += q2.Dequeue();
+            }
+
+            if (q2.Count == k)
+            {
+                result = Math.Min(result, sum * r);
+            }
+        }
+
+        return result;
+    }
+}
--- Tasks/Daily/README.md
+++ Tasks/Daily/README.md
@@ -195,7 +195,7 @@ Return _the_ `head` _of the modified linked list._
 - The number of the nodes in the given list is in the range `[1, 105]`.
 - `1 <= Node.val <= 105`
 
-### [786. K-th Smallest Prime Fraction](https://leetcode.com/problems/k-th-smallest-prime-fraction/)
+### 7. [786. K-th Smallest Prime Fraction](https://leetcode.com/problems/k-th-smallest-prime-fraction/)
 
 #Medium
 
@@ -228,3 +228,28 @@ The third fraction is 2/5.
 - `1 <= k <= arr.length * (arr.length - 1) / 2`
 
 **Follow up:** Can you solve the problem with better than `O(n2)` complexity?
+
+### 8. [Minimum Cost to Hire K Workers](https://leetcode.com/problems/minimum-cost-to-hire-k-workers/)
+
+#Hard
+
+There are `n` workers. You are given two integer arrays `quality` and `wage` where `quality[i]` is the quality of the `ith` worker and `wage[i]` is the minimum wage expectation for the `ith` worker.
+
+We want to hire exactly `k` workers to form a **paid group**. To hire a group of `k` workers, we must pay them according to the following rules:
+
+1. Every worker in the paid group must be paid at least their minimum wage expectation.
+2. In the group, each worker's pay must be directly proportional to their quality. This means if a worker’s quality is double that of another worker in the group, then they must be paid twice as much as the other worker.
+
+Given the integer `k`, return _the least amount of money needed to form a paid group satisfying the above conditions_. Answers within `10-5` of the actual answer will be accepted.
+
+**Example 1:**
+
+**Input:** quality = [10,20,5], wage = [70,50,30], k = 2
+**Output:** 105.00000
+**Explanation:** We pay 70 to 0th worker and 35 to 2nd worker.
+
+**Example 2:**
+
+**Input:** quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3
+**Output:** 30.66667
+**Explanation:** We pay 4 to 0th worker, 13.33333 to 2nd and 3rd workers separately.
